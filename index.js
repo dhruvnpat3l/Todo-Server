45c5f6d5a647c040b4c9cfc1fc7c02bc467b375c
Thunder-Emperor <bellerophonpegasus29@gmail.com>
2023-09-06 02:13:32 +0530

    Added index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/',(req,res)=>{
+    res.send("successfully connect to AWS")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`)
+})
