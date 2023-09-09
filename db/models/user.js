0e21bf81a2ce83db5c4c35f51babe37ee3d41b4c
Thunder-Emperor <bellerophonpegasus29@gmail.com>
2023-09-09 20:07:08 +0530

    Sign up completed

--- db/models/user.js
+++ db/models/user.js
@@ -2,6 +2,20 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
  //user Schema fields
+
+ username:{
+    type:String,
+    required:true,
+    unique:true,
+ },
+ password:{
+    type: String,
+    required: true,
+ },
+ createdAt:{
+    type:Date,
+    default:Date.now,
+ },
 })
 
 module.exports = mongoose.model('User', userSchema)
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const cors = require('cors')
+
+// All connection and database file 
 const connectDB = require('./db/connection');
+const  User = require('./db/models/user.js');
 
 const app = express()
+
+app.use(cors())
+app.use(bodyparser.json())
 
 connectDB();
 
@@ -12,6 +19,24 @@ const port = 3000
 app.get('/',(req,res)=>{
     res.send("successfully connect to AWS")
 })
+
+app.post('/signup',async(req,res)=>{
+    try{
+        const {username,password} = req.body;
+
+        const user = new User({
+            username,
+            password,
+            
+        });
+        await user.save();
+        res.status(201).json({message:'user registered successfully'});
+    }
+    catch (error){
+        console.error('Error while signing up',error)
+        res.status(500).json({message:'Internal server Error'});
+    }
+});
 
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`)
