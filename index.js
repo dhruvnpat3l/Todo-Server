f47711765ff130c9b4dea7ce3ba8fb41a384ac4f
Thunder-Emperor <bellerophonpegasus29@gmail.com>
2023-09-10 00:43:08 +0530

    Comment in index.js file

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ const connectDB = require('./db/connection');
 const  User = require('./db/models/user.js');
 
 const app = express()
+const port = 3000
 
+//// Enable CORS and JSON parsing
 app.use(cors())
 app.use(bodyparser.json())
 
+// Database Connection
 connectDB();
 
-const port = 3000
 
 
 app.get('/',(req,res)=>{
     res.send("successfully connect to AWS")
 })
 
+// // Define route for handling user signups 
 app.post('/signup',async(req,res)=>{
     try{
         const {username,password} = req.body;
@@ -38,6 +41,7 @@ app.post('/signup',async(req,res)=>{
     }
 });
 
+// listen port
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`)
 })
